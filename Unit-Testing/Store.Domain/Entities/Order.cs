3e53effa239a51c9bdf1afdd683065298745a21e
Michael Peter <michael_piterct@hotmail.com>
2021-02-17 01:02:19 -0300

    Order Entity

---
+++ Unit-Testing/Store.Domain/Entities/Order.cs
@@ -0,0 +1,58 @@
+﻿using Store.Domain.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace Store.Domain.Entities
+{
+    public class Order : Entity
+    {
+        public Order(Customer customer, decimal deliveryFee, Discount discount)
+        {
+            Customer = customer;
+            Date = DateTime.Now;
+            Number = Guid.NewGuid().ToString().Substring(0, 8);
+            Status = EOrderStatus.WaitingPayment;
+            DeliveryFee = deliveryFee;
+            Discount = discount;
+            Items = new List<OrderItem>();
+        }
+        public Customer Customer { get; private set; }
+        public DateTime Date { get; private set; }
+        public string Number { get; private set; }
+        public IList<OrderItem> Items { get; private set; }
+        public EOrderStatus Status { get; private set; }
+        public decimal DeliveryFee { get; private set; }
+        public Discount Discount { get; private set; }
+
+        public void AddItem(Product product, int quantity)
+        {
+            OrderItem item = new OrderItem(product, quantity);
+            Items.Add(item);
+        }
+
+        public decimal Total()
+        {
+            decimal total = 0;
+            foreach (var item in Items)
+            {
+                total += item.Total();
+            }
+
+            total += DeliveryFee;
+            total += Discount != null ? Discount.Value() : 0;
+
+            return total;
+        }
+
+        public void Pay(decimal amount)
+        {
+            if (amount == Total())
+                this.Status = EOrderStatus.WaitingDelivery;
+        }
+
+        public void Cancel()
+        {
+            this.Status = EOrderStatus.Canceled;
+        }
+    }
+}
---
+++ Unit-Testing/Store.Domain/Enums/EOrderStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Store.Domain.Enums
+{
+    public enum EOrderStatus
+    {
+        WaitingPayment = 1,
+        WaitingDelivery = 2,
+        Canceled = 3
+    }
+}
