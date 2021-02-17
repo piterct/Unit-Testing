ac68c8e7105cb11a36ccd3f44663ce8228b3b178
Michael Peter <michael_piterct@hotmail.com>
2021-02-17 00:16:32 -0300

    OrderItem entity

--- Unit-Testing/Store.Domain/Entities/Discount.cs
+++ Unit-Testing/Store.Domain/Entities/Discount.cs
@@ -11,7 +11,7 @@ namespace Store.Domain.Entities
         }
 
         public decimal Amount { get; private set; }
-        public DateTime ExpireDate { get; set; }
+        public DateTime ExpireDate { get; private set; }
 
         public bool IsValid()
         {
---
+++ Unit-Testing/Store.Domain/Entities/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿namespace Store.Domain.Entities
+{
+    public class OrderItem : Entity
+    {
+        public OrderItem(Product product, int quantity)
+        {
+            Product = product;
+            Price = Product != null ? product.Price : 0;
+            Quantity = quantity;
+        }
+
+        public Product Product { get; private set; }
+        public decimal Price { get; private set; }
+        public int Quantity { get; private set; }
+
+        public decimal Total()
+        {
+            return Price * Quantity;
+        }
+    }
+}
