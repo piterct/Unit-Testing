6026fbd9a398c0bcc27201f452a4e3af26fc9422
Michael Peter <michael_piterct@hotmail.com>
2021-02-17 11:19:31 -0300

    Status waiting payment Tests

--- Unit-Testing/Store.Tests/Entities/OrderTests.cs
+++ Unit-Testing/Store.Tests/Entities/OrderTests.cs
@@ -1,26 +1,36 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Store.Domain.Entities;
 using Store.Domain.Enums;
+using System;
 
 namespace Store.Tests.Entities
 {
     [TestClass]
     public class OrderTests
     {
+
+        private readonly Customer _customer;
+        private readonly Order _order;
+        private readonly Product _product;
+
+        public OrderTests()
+        {
+            _customer = new Customer("Michael Peter", "michael_piterct@hotmail.com");
+            _order = new Order(_customer, 0, null);
+            _product = new Product("Produto 1", 10, true);
+        }
         [TestMethod]
         [TestCategory("Domain")]
-        public void Have_a_new_valid_order_it_was_did_must_generate_a_number_with_8_characters()
+        public void Have_a_new_valid_order_it_was__must_generate_a_number_with_8_characters()
         {
-            var customer = new Customer("Michael Peter", "michael_piterct@hotmail.com");
-            var order = new Order(customer, 0, null);
-            Assert.AreEqual(8, order.Number.Length);
+            Assert.AreEqual(8, _order.Number.Length);
         }
 
         [TestMethod]
         [TestCategory("Domain")]
         public void Have_a_new_order_your_status_must_be_waiting_payment()
         {
-            Assert.Fail();
+            Assert.AreEqual(_order.Status, EOrderStatus.WaitingPayment);
         }
 
         [TestMethod]
