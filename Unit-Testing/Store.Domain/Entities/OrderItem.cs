69f7d8ba72c787ae505244f598a5bff4d48c7d19
Michael Peter <michael_piterct@hotmail.com>
2021-02-17 01:31:50 -0300

    Order item validation

--- Unit-Testing/Store.Domain/Entities/OrderItem.cs
+++ Unit-Testing/Store.Domain/Entities/OrderItem.cs
@@ -1,9 +1,18 @@
-﻿namespace Store.Domain.Entities
+﻿using Flunt.Validations;
+
+namespace Store.Domain.Entities
 {
     public class OrderItem : Entity
     {
         public OrderItem(Product product, int quantity)
         {
+            AddNotifications(
+             new Contract()
+             .Requires()
+             .IsNotNull(product, "Customer", "Invalid Product")
+             .IsGreaterThan(quantity, 0, "Quantity", "The quantity must be greater than zero")
+             );
+
             Product = product;
             Price = Product != null ? product.Price : 0;
             Quantity = quantity;
