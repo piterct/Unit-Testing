c12c33a8685d3db50a125a61a7ae492044bae173
Michael Peter <michael_piterct@hotmail.com>
2021-02-17 04:46:12 -0300

    OrderTests

---
+++ Unit-Testing/Store.Tests/Entities/OrderTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Store.Tests.Entities
+{
+    [TestClass]
+    public class OrderTests
+    {
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Had_a_new_valid_order_it_was_did_must_generate_a_number_with_8_characters()
+        {
+            Assert.Fail();
+        }
+    }
+}
