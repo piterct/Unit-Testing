302fe63700dbf349fbd09bdcdb28513c18de139d
Michael Peter <michael_piterct@hotmail.com>
2021-02-17 10:45:30 -0300

    Generate Order Test

--- Unit-Testing/Store.Tests/Entities/OrderTests.cs
+++ Unit-Testing/Store.Tests/Entities/OrderTests.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Domain.Entities;
+using Store.Domain.Enums;
 
 namespace Store.Tests.Entities
 {
@@ -9,12 +11,21 @@ namespace Store.Tests.Entities
         [TestCategory("Domain")]
         public void Have_a_new_valid_order_it_was_did_must_generate_a_number_with_8_characters()
         {
-            Assert.Fail();
+            var customer = new Customer("Michael Peter", "michael_piterct@hotmail.com");
+            var order = new Order(customer, 0, null);
+            Assert.AreEqual(8, order.Number.Length);
         }
 
         [TestMethod]
         [TestCategory("Domain")]
         public void Have_a_new_order_your_status_must_be_waiting_payment()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void Have_a_payment_order_your_status_must_be_waiting_delivery()
         {
             Assert.Fail();
         }
