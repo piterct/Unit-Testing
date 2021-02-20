05677e913f55900be3fa1c75b29c8afee66abdd3
Michael Peter <michael_piterct@hotmail.com>
2021-02-20 06:38:43 -0300

    Expression Queries Tests

---
+++ Unit-Testing/Store.Domain/Queries/ProductQueries.cs
@@ -0,0 +1,21 @@
+﻿using Store.Domain.Entities;
+using System;
+using System.Linq.Expressions;
+
+namespace Store.Domain.Queries
+{
+    public class ProductQueries
+    {
+
+        public static Expression<Func<Product, bool>> GetActiveProducts()
+        {
+            return x => x.Active;
+        }
+
+        public static Expression<Func<Product, bool>> GetInactiveProducts()
+        {
+            return x => x.Active == false;
+        }
+
+    }
+}
