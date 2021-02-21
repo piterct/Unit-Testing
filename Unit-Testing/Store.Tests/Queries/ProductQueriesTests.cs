10cba962f1d0969b5b3daf85f442613ae43f5e92
Michael Peter <michael_piterct@hotmail.com>
2021-02-20 21:57:55 -0300

    Queries Tests

---
+++ Unit-Testing/Store.Tests/Queries/ProductQueriesTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Domain.Entities;
+using Store.Domain.Queries;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Store.Tests.Queries
+{
+    [TestClass]
+    public class ProductQueriesTests
+    {
+        private IList<Product> _products;
+        public ProductQueriesTests()
+        {
+            _products = new List<Product>();
+            _products.Add(new Product("Produto 01", 10, true));
+            _products.Add(new Product("Produto 02", 20, true));
+            _products.Add(new Product("Produto 03", 30, true));
+            _products.Add(new Product("Produto 04", 40, false));
+            _products.Add(new Product("Produto 05", 50, false));
+        }
+
+        [TestMethod]
+        [TestCategory("Queries")]
+        public void There_is_a_query_about_actives_produts_must_return_3()
+        {
+            var result = _products.AsQueryable().Where(ProductQueries.GetActiveProducts());
+            Assert.AreEqual(result.Count(), 3);
+        }
+
+        [TestMethod]
+        [TestCategory("Queries")]
+        public void There_is_a_query_about_inactives_produts_must_return_2()
+        {
+            var result = _products.AsQueryable().Where(ProductQueries.GetInactiveProducts());
+            Assert.AreEqual(result.Count(), 2);
+        }
+    }
+}
