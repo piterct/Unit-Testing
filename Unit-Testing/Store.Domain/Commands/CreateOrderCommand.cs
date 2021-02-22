24bbee1d127be189515527e89871ad12da6776e5
Michael Peter <michael_piterct@hotmail.com>
2021-02-22 01:11:44 -0300

    CustomerOrderCommand

---
+++ Unit-Testing/Store.Domain/Commands/CreateOrderCommand.cs
@@ -0,0 +1,36 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using Store.Domain.Commands.Interfaces;
+using System.Collections.Generic;
+
+namespace Store.Domain.Commands
+{
+    public class CreateOrderCommand : Notifiable, ICommand
+    {
+        public CreateOrderCommand()
+        {
+            Items = new List<CreateOrderItemCommand>();
+        }
+
+        public CreateOrderCommand(string customer, string zipCode, string promoCode, IList<CreateOrderItemCommand> items)
+        {
+            Customer = customer;
+            ZipCode = zipCode;
+            PromoCode = promoCode;
+            Items = items;
+        }
+
+        public string Customer { get; set; }
+        public string ZipCode { get; set; }
+        public string PromoCode { get; set; }
+        public IList<CreateOrderItemCommand> Items { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasLen(Customer, 11, "Customer", "Invalid Customer")
+                .HasLen(ZipCode, 8, "ZipCode", "Invalid Zipcode"));
+        }
+    }
+}
---
+++ Unit-Testing/Store.Domain/Commands/CreateOrderItemCommand.cs
@@ -0,0 +1,29 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using Store.Domain.Commands.Interfaces;
+using System;
+
+namespace Store.Domain.Commands
+{
+    public class CreateOrderItemCommand : Notifiable, ICommand
+    {
+        public CreateOrderItemCommand() { }
+
+        public CreateOrderItemCommand(Guid product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+
+        public Guid Product { get; set; }
+        public int Quantity { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasLen(Product.ToString(), 32, "Product", "Invalid Product")
+                .IsGreaterThan(Quantity, 0, "Quantity", "The quantity must be greater than zero"));
+        }
+    }
+}
