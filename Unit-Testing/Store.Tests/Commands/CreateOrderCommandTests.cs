14fde8f2be6d833e7ee0f45ddb382459ba147cc7
Michael Peter <michael_piterct@hotmail.com>
2021-02-23 03:36:05 -0300

    Create Order Command  Tests

---
+++ Unit-Testing/Store.Tests/Commands/CreateOrderCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Domain.Commands;
+using System;
+
+namespace Store.Tests.Commands
+{
+    [TestClass]
+    public class CreateOrderCommandTests
+    {
+        [TestMethod]
+        [TestCategory("Handlers")]
+        public void There_is_an_invalid_command_doesnt_create_order()
+        {
+            var command = new CreateOrderCommand();
+            command.Customer = "";
+            command.ZipCode = "131100782";
+            command.PromoCode = "215445";
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Validate();
+
+            Assert.AreEqual(command.Valid, false);
+        }
+    }
+}
